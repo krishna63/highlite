3f365881b2b7276081ac590cbad6296e0368b766
Chakri <chakri.gudboy@gmail.com>
2013-09-20 15:26:53 +0530

    Made major changes

--- js/jquery.highlite.js
+++ js/jquery.highlite.js
@@ -1,22 +1,25 @@
 (function($) {
     //get the highlighted text
 	function getSelectionText() {
- 	   var text = "";
-    	if (window.getSelection) {
-       		 text = window.getSelection().toString();
-    	} else if (document.selection && document.selection.type != "Control") {
-	        text = document.selection.createRange().text;
-    	}
-    	return text;
+ 	    var parentEl = null, sel;
+   		if (window.getSelection) {
+        sel = window.getSelection();
+        console.log(sel.toString());
+        sel = sel.toString();
+     	    if(sel.length > 1){
+    		    	var spn = '<span class="selected">' + sel + '</span>';
+			        document.activeElement.innerHTML = document.activeElement.innerHTML.replace (sel, spn);
+	        	}
+        	}
 	}
 	// jQuery plugin definition
 	$.fn.highlite = function(params) {
 		$("*").keypress(function(evnt){
 			if(evnt.shiftKey && evnt.which == 72){
 				console.log("init highlighted");
-				alert(getSelectionText());
+				getSelectionText();
 			}
+			$('span.selected').css({"background-color":"yellow"});
 		});
 	};
-
 })(jQuery);

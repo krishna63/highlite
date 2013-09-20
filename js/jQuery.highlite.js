d603fee06b213629ce86d97ee6c4df222afd4077
Chakri <chakri.gudboy@gmail.com>
2013-09-20 14:54:49 +0530

    Made changes to getSelctionText

--- js/jQuery.highlite.js
+++ js/jQuery.highlite.js
@@ -1,8 +1,22 @@
 (function($) {
-
+    //get the highlighted text
+	function getSelectionText() {
+ 	   var text = "";
+    	if (window.getSelection) {
+       		 text = window.getSelection().toString();
+    	} else if (document.selection && document.selection.type != "Control") {
+	        text = document.selection.createRange().text;
+    	}
+    	return text;
+	}
 	// jQuery plugin definition
 	$.fn.highlite = function(params) {
-		
+		$("*").keypress(function(evnt){
+			if(evnt.shiftKey && evnt.which == 72){
+				console.log("init highlighted");
+				alert(getSelectionText());
+			}
+		});
 	};
 
 })(jQuery);
